178f29a1d61418757fd495d3bc8a7acdd7946e95
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-07-05 17:41:31 -0300

    feat/ add conditional render to header

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -6,10 +6,10 @@
       Express Forms
     </v-app-bar-title>
     <v-spacer></v-spacer>
-    <v-text-field v-model="search" label="Search"></v-text-field>
+    <v-text-field v-if="isEditorMode" v-model="search" label="Search"></v-text-field>
     <!-- v-model="search" -->
     <v-spacer></v-spacer>
-    <v-btn size="large" prepend-icon="mdi-plus">Criar novo form</v-btn>
+    <v-btn v-if="isEditorMode" size="large" prepend-icon="mdi-plus">Criar novo form</v-btn>
 
   </v-app-bar>
 </template>
@@ -21,7 +21,17 @@
 export default {
   data: () => ({
     search: '',
+    isEditorMode: false,
   }),
+  watch: {
+    $route(to) {
+      if (to.fullPath.match(/\/forms\/[0-9]+/)) {
+        this.isEditorMode = false;
+      } else {
+        this.isEditorMode = true;
+      }
+    },
+  },
 }
 </script>
 
